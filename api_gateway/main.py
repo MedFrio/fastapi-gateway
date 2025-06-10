3a91451b7600926b37375fe71438809d9b11534b
Mohammed FRIOUICHEN <98142644+MedFrio@users.noreply.github.com>
2025-06-10 10:30:55 +0200

    Update main.py

--- api_gateway/main.py
+++ api_gateway/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
 from services import auth, orders, kitchen, delivery
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(title="API Gateway - Restaurant")
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 app.include_router(orders.router)
